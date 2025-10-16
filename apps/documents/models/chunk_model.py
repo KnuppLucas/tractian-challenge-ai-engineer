616fe6bdb5307643ccaa80cc8cdf379f436c793a
Lucas Knupp <lucas_knupp@outlook.com>
2025-10-16 17:17:28 -0300

    feat: add chunk model at the database with django db

---
+++ apps/documents/models/chunk_model.py
@@ -0,0 +1,22 @@
+import uuid
+from django.db import models
+
+from apps.documents.models.document_model import Document
+
+class Chunk(models.Model):
+    """
+    Modelo responsável por armazenar segmentos (chunks) de texto extraídos de documentos.
+    - Cada chunk representa uma parte do texto de um documento processado.
+    - Utilizado para operações de embedding e busca semântica.
+    - Mantém a referência ao documento original e a ordem dos trechos.
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    document = models.ForeignKey(Document, related_name="chunks", on_delete=models.CASCADE)
+    text = models.TextField()
+    order = models.IntegerField()
+
+    class Meta:
+        db_table = "chunks"
+
+    def __str__(self):
+        return f"Chunk {self.order} from {self.document.filename}"
