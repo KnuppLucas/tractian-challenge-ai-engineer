e8f894234122bcc6573f4973f3508951d82a7292
Lucas Knupp <lucas_knupp@outlook.com>
2025-10-16 17:14:20 -0300

    ci: add django and enviroments configs

---
+++ config/settings/base.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+SECRET_KEY = "fake-key-for-dev"
+DEBUG = True
+ALLOWED_HOSTS = ["*"]
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    "rest_framework",
+    "apps.documents",
+    "apps.questions",
+]
+
+MIDDLEWARE = [
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+]
+
+ROOT_URLCONF = "config.urls"
+WSGI_APPLICATION = "config.wsgi.application"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "tractian_db",
+        "USER": "tractian_user",
+        "PASSWORD": "tractian_pass",
+        "HOST": "db",
+        "PORT": "5432",
+    }
+}
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
+USE_TZ = True
+STATIC_URL = "/static/"
